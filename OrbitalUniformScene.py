a08d77202e0a3a6fe8044a527017087416386a6f
noceive <nocivescience@gmail.com>
2022-12-18 00:07:38 -0300

    mucho mejor

--- OrbitalUniformScene.py
+++ OrbitalUniformScene.py
@@ -1,98 +1,46 @@
 from manim import *
-class Orbitals(Scene):
+class ElectronScene(Scene):
     CONFIG={
-        "center":ORIGIN,
-        "radius":[1,2,3],
-        "numbers":[2,8,8],
-        "dot_style":{
-            "radius":0.08,
-            "color":interpolate_color(RED,YELLOW,np.random.random())
-        },
-        "dot_center_style":{
-            "color":WHITE,
-            "radius":0.2,
-        }
+        'center':ORIGIN+UP
     }
-
     def construct(self):
-        self.get_circle(self.CONFIG['radius'])
-        mobs=VGroup()
-        for t in range(len(self.CONFIG['radius'])):
-            mob=self.get_points(self.CONFIG['radius'][t],num=self.CONFIG['numbers'][t])
-            mobs.add(mob)
-        textos=VGroup()
-        for t in range(len(self.CONFIG['radius'])):
-            texto=self.get_labels(mobs[t],t)
-            textos.add(texto)
-        self.play(*list(map(Create,[mobs])),run_time=4)
-        self.play(LaggedStartMap(Write,textos))
+        self.get_dots(1,8)
         self.wait()
-        time=0
-        while time<15:
-            pre_time=2
-            time+=pre_time
-            self.play(
-                *[self.get_ubication_points(t,self.CONFIG['radius'][0],np.random.uniform(0,2*np.pi)) for t in mobs[0]],
-                *[self.get_ubication_points(t,self.CONFIG['radius'][1],np.random.uniform(0,2*np.pi)) for t in mobs[1]],
-                *[self.get_ubication_points(t,self.CONFIG['radius'][2],np.random.uniform(0,2*np.pi)) for t in mobs[2]],
-                *[self.get_labels_update(mobs[t],textos[t]) for t in range(len(self.CONFIG['radius']))],
-                run_time=pre_time,
-                rate_func=double_smooth
-            )
-
-    def get_points(self,radius,num):
+    def get_dots(self,radius,num):
         points=np.array([
-            self.CONFIG['center']+rotate_vector(radius*RIGHT,theta) for theta in np.random.uniform(0,2*np.pi,num)
+            self.CONFIG['center']+rotate_vector(radius*RIGHT,theta)
+            for theta in np.linspace(0,TAU,num)
         ])
         dots=VGroup()
-        for point in points:
-            dot=Dot(**self.CONFIG['dot_style']).move_to(point)
+        texts=VGroup()
+        for i in range(len(points)):
+            dot=Dot().move_to(points[i])
+            text=MathTex('e_%g' %i)
+            dot.text=text
+            text.move_to(dot.get_center())
+            vect=dot.get_center()-self.CONFIG['center']
+            vect/=np.linalg.norm(vect)
+            text.shift(vect*.4)
+            texts.add(text)
             dots.add(dot)
-        return dots
-    
-    def get_circle(self,radius):
-        dot_center=Dot(self.CONFIG['center'],**self.CONFIG['dot_center_style'])
-        texto=MathTex("+").move_to(dot_center.get_center())
-        texto.set_color(BLACK).set_stroke(width=1,color=BLACK)
-        texto.set_width(dot_center.get_width())
-        dot_center.add(texto)
-        self.add_foreground_mobjects(dot_center,texto)
-        self.play(Create(dot_center))
-        for radio in radius:
-            radii=DashedLine(dot_center.get_center(),RIGHT*radio)
-            self.play(Create(radii))
-            circle=Circle(color=BLUE,radius=radio)
-            self.play(Rotate(radii,angle=TAU,about_point=dot_center.get_center())\
-                ,Create(circle),run_time=1,rate_func=smooth)
-            self.play(Create(radii,rate_func=lambda t: 1-t))
-        self.wait()
-
-    def get_ubication_points(self,mob,radius,theta):
-        curr_theta=angle_of_vector(mob.get_center())
-        new_theta=(curr_theta+theta)%(2*np.pi)-2*np.pi
-        def get_update(mob,alpha):
-            theta=interpolate(curr_theta,new_theta,alpha)
-            mob.move_to(radius*(np.cos(theta)*RIGHT+np.sin(theta)*UP))
-            return mob
-        return UpdateFromAlphaFunc(mob,get_update)
-    
-    def get_labels(self,mobs,m):
-        point_labels=VGroup()
-        for t,mob in zip(range(self.CONFIG['numbers'][m]),mobs):
-            point_label=MathTex("e_%i" %(t+1)).scale(0.6)
-            point_label.move_to(mob)
+        dots[0].set_opacity(0)
+        texts[0].set_opacity(0)
+        self.play(Create(dots),Write(texts))
+        def update_dot(mob):
+            mob.text.move_to(mob.get_center()) #poner .get_center()
             vect=mob.get_center()-self.CONFIG['center']
             vect/=np.linalg.norm(vect)
-            point_label.shift(vect*0.26)
-            point_labels.add(point_label)
-        return point_labels
-
-    def get_labels_update(self,point_mobs,labels):
-        def update_labels(labels):
-            for label, mob in zip(labels,point_mobs):
-                label.move_to(mob)
-                vect=mob.get_center()-self.CONFIG['center']
-                vect/=np.linalg.norm(vect)
-                label.shift(vect*0.26)
-            return label
-        return UpdateFromFunc(labels,update_labels)
+            mob.text.shift(vect*.4)
+        for dot in dots:
+            dot.add_updater(update_dot)
+            curr_theta=angle_of_vector(dot.get_center()) #poner .get_center()
+            new_theta=TAU*4+angle_of_vector(dot.get_center())
+            def update_alpha(mob,alpha):
+                theta=interpolate(curr_theta,new_theta,alpha)
+                mob.move_to(
+                    radius*np.cos(theta)*RIGHT+radius*np.sin(theta)*UP
+                )
+            self.play(
+                UpdateFromAlphaFunc(dot,update_alpha),
+                run_time=1
+            )
